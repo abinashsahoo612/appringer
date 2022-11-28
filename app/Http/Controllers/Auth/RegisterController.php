ed2c87676de2a715262bd54de492e24945bc0a27
abinashsahoo612 <abinashsahoo612@gmail.com>
2022-11-28 12:13:13 +0530

    issue fixed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use App\Models\Books;
+use GuzzleHttp\Client;
 
 class RegisterController extends Controller
 {
@@ -24,11 +27,30 @@ class RegisterController extends Controller
         $user->name = $request->input('name');
         $user->email = $request->input('email');
         $user->password = Hash::make( $request->input('password'));
-        $user->save();
+        if($user->save()){
+            $books = DB::table('books')->select('authors', 'title', 'subtitle', 'thumbnail', 'small_thumbnail')->get();
+            Auth::login($user);
+            if (count($books) == 0) {
+                $client = new Client();
+                $storeBooks = new Books();
+                $res = $client->get('https://run.mocky.io/v3/821d47eb-b962-4a30-9231-e54479f1fbdf', ['headers' =>  ['Authorization', 'AppRinger']]);
+
+                $data = json_decode($res->getBody()->getContents());
+                foreach ($data->items as $key => $value) {
+                    $storeBooks = new Books();
+                    $storeBooks->authors = $value->volumeInfo->authors[0];
+                    $storeBooks->title = $value->volumeInfo->title;
+                    $storeBooks->subtitle = isset($value->volumeInfo->subtitle) ? $value->volumeInfo->subtitle : '';
+                    $storeBooks->thumbnail = $value->volumeInfo->imageLinks->thumbnail;
+                    $storeBooks->small_thumbnail = $value->volumeInfo->imageLinks->smallThumbnail;
+                    $storeBooks->save();
+                }
+                $books = DB::table('books')->select('authors', 'title', 'subtitle', 'thumbnail', 'small_thumbnail')->get();
+            }
+        }
 
         Auth::login($user);
-
-        return redirect('/home');
+        return view('/home')->with('books', $books);
 
     }
 }
